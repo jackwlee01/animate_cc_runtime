28d6b9612bcf7b91d459f44c8610083709321899
Jack Lee <jackwlee01@gmail.com>
2022-01-13 11:52:53 +1100

    Clean up example code

--- src/examples/example-utils.ts
+++ src/examples/example-utils.ts
@@ -2,44 +2,47 @@ import { Clip } from "../core/Clip";
 import { Library } from "../core/Library";
 
 
-
 export function setupCanvas(canvas:HTMLCanvasElement) {
-    const buffer = canvas.getContext('2d')!
+    // Set the context quality and smoothing
+    const ctx = canvas.getContext('2d')!
+    ctx.imageSmoothingEnabled = true;
+    ctx.imageSmoothingQuality = 'high'
 
-    buffer.imageSmoothingEnabled = true;
-    buffer.imageSmoothingQuality = 'high'
-
-    // Get the device pixel ratio, falling back to 1.
-    
-   var bodyRec = document.body.getBoundingClientRect();
-
+    // Set the canvas according to window width
+    var bodyRec = document.body.getBoundingClientRect();
     canvas.width = Math.min(1000, bodyRec.width - 8);
     canvas.height = canvas.width;
 
-    var dpr = window.devicePixelRatio || 1;
+    // Get the device pixel ratio, falling back to 1.
+    let dpr = window.devicePixelRatio || 1;
 
     // Get the size of the canvas in CSS pixels.
-    var rect = canvas.getBoundingClientRect();
+    let rect = canvas.getBoundingClientRect();
+    
     // Give the canvas pixel dimensions of their CSS
     // size * the device pixel ratio.
-
     canvas.style.border = "2px solid gray";
     canvas.style.width = "" + canvas.width + "px";
     canvas.style.height = "" + canvas.height + "px";
     canvas.width = rect.width * dpr;
     canvas.height = rect.height * dpr;
-    
-    // Scale all drawing operations by the dpr, so you
-    // don't have to worry about the difference.
-    //ctx.scale(dpr, dpr);
-    //console.log(dpr)
-    //return ctx;
+
     return dpr;
 }
 
 
 
-export function addExampleButtons(lib:Library, onClick:(nextSymbol:Clip)=>void){
+export function addExampleButtons(lib:Library, onClick:(nextSymbol:Clip)=>void, onMinus:()=>void, onPlus:()=>void){
+    const minusButton = document.createElement('button')
+    minusButton.innerHTML = '-'
+    minusButton.onclick = onMinus
+    document.getElementById("buttons")!.appendChild(minusButton);
+
+    const plusButton = document.createElement('button')
+    plusButton.innerHTML = '+'
+    plusButton.onclick = onPlus
+    document.getElementById("buttons")!.appendChild(plusButton);
+
     for(const clip of lib.clips){
         if(clip.name.indexOf("/")!=-1 || clip.name.indexOf("Symbol ")==0 || clip.name.indexOf("Tween ")==0 || clip.name.indexOf("/Symbol ")!=-1 || clip.name.indexOf("/Tween ")!=-1) continue;
         var button = document.createElement("button")
--- src/examples/test-page.ts
+++ src/examples/test-page.ts
@@ -1,41 +1,35 @@
 import { addExampleButtons, setupCanvas } from "./example-utils";
 import { Canvas2dAnimationContext } from "../Canvas2dAnimationContext";
+import { Clip } from "../core/Clip";
+
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement
 const ctx = canvas.getContext('2d')!
-const animContext = new Canvas2dAnimationContext(ctx)
+var dpr = setupCanvas(canvas) // Device pixel ratio
 
+// Set up animation context and libraries
+const animContext = new Canvas2dAnimationContext(ctx)
 const libraries = {
     test: animContext.createLibrary('test', 'test')
 }
 
 
 
-// Device pixel ratio
-var dpr = setupCanvas(canvas)
-
-var colsAndRows = 1;
-
-
-const minusButton = document.createElement('button')
-minusButton.innerHTML = '-'
-minusButton.onclick = () => { if(colsAndRows > 1) colsAndRows-- };
-document.getElementById("buttons")!.appendChild(minusButton);
-
-const plusButton = document.createElement('button')
-plusButton.innerHTML = '+'
-plusButton.onclick = () => colsAndRows++;
-document.getElementById("buttons")!.appendChild(plusButton);
-
-
-
 async function init(){
+    // Load the libraries
     await libraries.test.loadData();
-    addExampleButtons(libraries.test, (nextSymbol)=>symbol=nextSymbol)
-    
+
+    // Example state
     let frame = 0;
     let symbol = libraries.test.clips[0]
+    var colsAndRows = 1;
 
+    // Set up example ui
+    const onSymbolPicked = (nextSymbol:Clip) => symbol=nextSymbol
+    const onMinus =  () => { if(colsAndRows > 1) colsAndRows-- }
+    const onPlus =  () => colsAndRows++
+    addExampleButtons(libraries.test, onSymbolPicked, onMinus, onPlus)
+    
     
     function update(){
         ctx.fillStyle = '#cccccc'
