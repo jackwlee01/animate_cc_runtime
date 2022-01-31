e91eba903b56d9fe048fd597da0c9bd0ed19ee2e
Jack Lee <jackwlee01@gmail.com>
2022-01-31 13:38:46 +1100

    Added some library tests

--- src/core/Library.test.ts
+++ src/core/Library.test.ts
@@ -1,7 +1,6 @@
 import { mockAtlas, mockLib } from "./test/mocks"
 
 
-
 test("new Library", () => {
     const lib = mockLib()
     
@@ -18,4 +17,31 @@ test("createAtlas", () => {
     const atlas = mockAtlas()
     expect(atlas).not.toBeNull()
     expect(atlas.library).not.toBeNull()
+})
+
+
+test("createClip", () => {
+    const lib = mockLib()
+    const clip = lib.createClip({
+        name: "ClipA"
+    })
+    expect(lib.clips.length).toEqual(1)
+    expect(lib.clipsByName['ClipA']).toBe(clip)
+    expect(lib.symbol("ClipA")).toBe(clip)
+})
+
+
+test("createSprite", () => {
+    const atlas = mockAtlas()
+    const lib = atlas.library;
+    const sprite = lib.createSprite(atlas, {
+        name: "0001",
+        x: 100,
+        y: 100,
+        width: 200,
+        height: 200,
+        rotated: false,
+    })
+    expect(lib.symbol("0001")).toBe(sprite)
+    expect(lib.spritesByName["0001"]).toBe(sprite)
 })
