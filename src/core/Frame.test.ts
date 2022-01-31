1065d91bbea131df9ae9e77c52d7de2d09ddfa56
Jack Lee <jackwlee01@gmail.com>
2022-01-31 15:20:43 +1100

    Test instance next and prev functionality

--- src/core/Frame.test.ts
+++ src/core/Frame.test.ts
@@ -2,13 +2,49 @@ import { Matrix } from "./geom/Matrix"
 import { mockAtlas, mockFrame, mockSprite } from "./test/mocks"
 
 test('createClipInstance', () => {
-    const frame = mockFrame()
-    const clip = frame.library.createClip({ name: "ClipB" })
-    const instance = frame.createClipInstance({
+    const frameA = mockFrame()
+    const clip = frameA.library.createClip({ name: "ClipB" })
+    const instanceA = frameA.createClipInstance({
         itemName: clip.name,
         name: '0001',
     })
-    expect(frame.instances[0]).toBe(instance)
+    expect(frameA.instances[0]).toBe(instanceA)
+
+    expect(instanceA.next).toBeUndefined()
+    expect(instanceA.prev).toBeUndefined()
+
+    const frameB = frameA.layer.createFrame({index:frameA.totalFrames, totalFrames: 10, name:'0'})
+    const instanceB = frameB.createClipInstance({
+        itemName: clip.name,
+        name: '0001',
+    })
+
+    expect(instanceA.next).toBe(instanceB)
+    expect(instanceA.prev).toBeUndefined()
+    expect(instanceB.next).toBeUndefined()
+    expect(instanceB.prev).toBe(instanceA)
+
+    const frameC = frameA.layer.createFrame({index:frameB.index+frameB.totalFrames, totalFrames: 10, name:'0'})
+    const instanceC = frameC.createClipInstance({
+        itemName: clip.name,
+        name: '0001',
+    })
+
+    expect(instanceA.next).toBe(instanceB)
+    expect(instanceA.prev).toBeUndefined()
+    expect(instanceB.next).toBe(instanceC)
+    expect(instanceB.prev).toBe(instanceA)
+    expect(instanceC.prev).toBe(instanceB)
+    expect(instanceC.next).toBeUndefined()
+
+    const frameD = frameA.layer.createFrame({index:frameC.index+frameC.totalFrames, totalFrames: 10, name:'0'})
+    const otherClip = frameA.library.createClip({ name: "ClipC" })
+    const instanceD = frameD.createClipInstance({
+        itemName: otherClip.name,
+        name: '0002'
+    })
+    expect(instanceC.next).toBeUndefined()
+    expect(instanceD.prev).toBeUndefined()
 })
 
 
