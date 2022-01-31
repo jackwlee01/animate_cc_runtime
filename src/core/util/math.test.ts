1d2da26177e52cbf9b7759e2f4892f45013e51f2
Jack Lee <jackwlee01@gmail.com>
2022-01-31 13:31:47 +1100

    Another test case for modWrap

--- src/core/util/math.test.ts
+++ src/core/util/math.test.ts
@@ -1,6 +1,7 @@
 import { modWrap } from "./math"
 
 test('modWrap', () => {    
+    expect(modWrap(0, 2)).toEqual(0)
     expect(modWrap(4, 2)).toEqual(0)
     expect(modWrap(5, 2)).toEqual(1)
     expect(modWrap(-4, 2)).toEqual(0)
